9e056460fc7072283be01f40269366b023a9203c
ardavey <ardavey@gmail.com>
2017-07-05 03:02:07 +0100

    Various tweaks and restrictions on RTs

--- EdinburghWatch.pl
+++ EdinburghWatch.pl
@@ -3,13 +3,15 @@
 use strict;
 use warnings;
 
-use 5.010;
+use 5.010001;
 
 use Getopt::Std;
 use File::Slurp;
 use Net::Twitter;
+use List::Util qw( shuffle );
 
 use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
 
 use lib qw(
   /home/ardavey/perl5/lib/perl5
@@ -44,32 +46,53 @@ my @statuses;
 my $r;
 
 $r = $tw->search( {
-  count => 5,
+  count => $c->{search_count},
   result_type => 'recent',
   since_id => $latest_id,
   geocode => '55.9485613,-3.2004082,10km',
 } );
 
-@statuses = sort { $a->{id} cmp $b->{id} } @{ $r->{statuses} };
+#@statuses = sort { $a->{id} cmp $b->{id} } @{ $r->{statuses} };
+@statuses = @{ $r->{statuses} };
 
 say "Search returned " . scalar @statuses . " status(es).";
 
+my $rt_count = 0;
+
 if ( scalar @statuses ) {
-  STATUS: foreach my $s ( @statuses ) {
-        
-    #say Dumper($s);
-    foreach my $un ( @{ $c->{blacklist} } ) {
-      if ( lc( $s->{user}{screen_name} ) eq lc( $un ) ) {
-        say "Skipping blacklisted user $un";
-        next STATUS;
-      }
+  STATUS: foreach my $s ( shuffle( @statuses ) ) {
+    
+    # Skip potentially sensitive content
+    if ( $s->{possibly_sensitive} ) {
+      say "Skipping potentially sensitive content in $s->{id}";
+      next STATUS;
     }
     
-    if ( $s->{in_reply_to_user_id} || $s->{in_reply_to_status_id} || $s->{text} =~ m/^@/s ) {
+    # Skip if it's not flagged as English
+    if ( $s->{lang} ne 'en' ) {
+      say "Skipping non-English tweet: $s->{id}";
+      next STATUS;
+    }
+    
+    # Skip if the tweet looks like a RT
+    if ( $s->{retweeted} || $s->{text} =~ m/^RT[: ]/ ) {
+      say "Skipping RT $s->{id}";
+      next STATUS;
+    }
+    
+    # Skip if the tweet looks like a reply
+    if ( $s->{in_reply_to_user_id} || $s->{in_reply_to_status_id} || $s->{text} =~ m/^@/ ) {
       say "Skipping reply $s->{id}";
       next STATUS;
     }
     
+    # Skip if the user is on the naughty list
+    if ( grep { /$s->{user}{screen_name}/i } @{ $c->{user_blacklist} } ) {
+      say "Skipping blacklisted user $s->{user}{screen_name}";
+      next STATUS;
+    }
+    
+    # Skip tweets with phrases on the banned list
     foreach my $phrase ( @{ $c->{banned_phrases} } ) {
       if ( $s->{text} =~ m/$phrase/si ) {
         say "Skipping banned phrase: $phrase";
@@ -77,15 +100,22 @@ if ( scalar @statuses ) {
       }
     }
 
-    say "Retweeting id $s->{id}";
+    print "ID $s->{id}: ";
+    
     # Bit of a hack here, but if a tweet has already been retweeted it throws a fatal error
     eval {
       $tw->retweet( { id => $s->{id} } );
     };
     
     if ( $@ ) {
-      say "RT error: $@";
+      say "Error: $@";
     }
+    else {
+      say "Retweeted";
+      $rt_count++;
+    }
+    
+    last STATUS if ( $rt_count == $c->{rts_per_run} );
   }
 }
 else {
